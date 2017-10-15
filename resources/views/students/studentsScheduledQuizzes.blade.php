c99b0e8ce3f4715f385df832bf2a8f1630cf3465
superjaze <jazelenedevera@gmail.com>
2017-10-15 11:20:22 +0800

    Changes: - studentsScheduledQuizzes - studentsLesson

--- resources/views/students/studentsScheduledQuizzes.blade.php
+++ resources/views/students/studentsScheduledQuizzes.blade.php
@@ -12,7 +12,7 @@
     </p>
     <hr />
     <div class="row">
-      <div class="col-12 col-md-3 col-lg-3 mt-4">
+  <!--   <div class="col-12 col-md-3 col-lg-3 mt-4">
         <div class="card border-silver">
           <div class="card-body bg-whitesmoke">
             <div><h4 class="card-title text-center">Quiz 1</h4></div>
@@ -28,8 +28,8 @@
               </a>
           </div>
         </div>
-      </div>
-      <div class="col-12 col-md-3 col-lg-3 mt-4">
+      </div> -->
+  <!--    <div class="col-12 col-md-3 col-lg-3 mt-4">
         <div class="card border-silver">
           <div class="card-body bg-whitesmoke">
             <div><h4 class="card-title text-center">Quiz 2</h4></div>
@@ -45,8 +45,8 @@
               </a>
           </div>
         </div>
-      </div>
-      <div class="col-12 col-md-3 col-lg-3 mt-4">
+      </div> -->
+<!--      <div class="col-12 col-md-3 col-lg-3 mt-4">
         <div class="card border-silver">
           <div class="card-body bg-whitesmoke">
             <div><h4 class="card-title text-center">Quiz 3</h4></div>
@@ -62,8 +62,8 @@
               </a>
           </div>
         </div>
-      </div>
-      <div class="col-12 col-md-3 col-lg-3 mt-4">
+      </div> -->
+<!--      <div class="col-12 col-md-3 col-lg-3 mt-4">
         <div class="card border-silver">
           <div class="card-body bg-whitesmoke">
             <div><h4 class="card-title text-center">Quiz 4</h4></div>
@@ -79,7 +79,65 @@
               </a>
           </div>
         </div>
+      </div> -->
+
+      <div class="col-sm-3">
+        <div class="card border-info">
+          <div class="card-body text-white bg-info">
+            <h4 class="card-title"><span class="oi oi-spreadsheet" title="scheduledQuiz" aria-hidden="true"></span> Quiz 1</h4>
+            <h6><span class="card-text oi oi-book"></span> SQL Introduction</h6>
+            <h6><span class="card-text oi oi-calendar"></span> September 4, 2017</h6>
+            <h6><span class="card-text oi oi-clock"></span> 9:00 AM</h6>
+          </div>
+          <a href="studentsTakeQuiz"><div class="card-footer border-info">
+            Take Quiz! <span class="oi oi-arrow-circle-right" title="viewScheduledQuizzes" aria-hidden="true"></span>
+          </div>
+          </a>
+        </div>
       </div>
+      <div class="col-sm-3">
+        <div class="card border-info">
+          <div class="card-body text-white bg-info">
+            <h4 class="card-title"><span class="oi oi-spreadsheet" title="classList" aria-hidden="true"></span> Quiz 2</h4>
+            <h6><span class="card-text oi oi-book"></span> SQL Fundamentals</h6>
+            <h6><span class="card-text oi oi-calendar"></span> November 7, 2017</h6>
+            <h6><span class="card-text oi oi-clock"></span> 10:00 AM</h6>
+          </div>
+          <a href="studentsTakeQuiz"><div class="card-footer border-info">
+            Take Quiz! <span class="oi oi-arrow-circle-right" title="viewScheduledQuizzes" aria-hidden="true"></span>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card border-info">
+          <div class="card-body text-white bg-info">
+            <h4 class="card-title"><span class="oi oi-spreadsheet" title="classList" aria-hidden="true"></span> Quiz 3</h4>
+            <h6><span class="card-text oi oi-book"></span> SQL DML</h6>
+            <h6><span class="card-text oi oi-calendar"></span> March 15, 2018</h6>
+            <h6><span class="card-text oi oi-clock"></span> 11:00 AM</h6>
+          </div>
+          <a href="studentsTakeQuiz"><div class="card-footer border-info">
+            Take Quiz! <span class="oi oi-arrow-circle-right" title="viewScheduledQuizzes" aria-hidden="true"></span>
+          </div>
+          </a>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="card border-info">
+          <div class="card-body text-white bg-info">
+            <h4 class="card-title"><span class="oi oi-spreadsheet" title="classList" aria-hidden="true"></span> Quiz 4</h4>
+            <h6><span class="card-text oi oi-book"></span> SQL DDL</h6>
+            <h6><span class="card-text oi oi-calendar"></span> February 8, 2018</h6>
+            <h6><span class="card-text oi oi-clock"></span> 12:00 AM</h6>
+          </div>
+          <a href="studentsTakeQuiz"><div class="card-footer border-info">
+            Take Quiz! <span class="oi oi-arrow-circle-right" title="viewScheduledQuizzes" aria-hidden="true"></span>
+          </div>
+          </a>
+        </div>
+      </div>
+
     </div>
   </div>
 </div>
